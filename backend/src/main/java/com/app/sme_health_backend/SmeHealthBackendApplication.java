b50f8e9cc139d1bb65ce397a476580f147d93857
aviongg <sulemanadil67@gmail.com>
2026-09-05 06:52:03 +0500

    chore: initialize project and backend

---
+++ backend/src/main/java/com/app/sme_health_backend/SmeHealthBackendApplication.java
@@ -0,0 +1,13 @@
+package com.app.sme_health_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmeHealthBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmeHealthBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/app/sme_health_backend/SmeHealthBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.sme_health_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmeHealthBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
